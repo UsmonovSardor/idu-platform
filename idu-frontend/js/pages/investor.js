d36e796f7eae79503208142bbfc5647643d177b8
Sardor <158450983+UsmonovSardor@users.noreply.github.com>
2026-04-11 22:19:39 +0500

    arch: pages/investor.js

---
+++ idu-frontend/js/pages/investor.js
@@ -0,0 +1,8 @@
+'use strict';
+// IDU Platform — pages/investor.js
+
+function fillInvestor(l,p,c){
+  document.getElementById('iLogin').value=l;
+  document.getElementById('iPass').value=p;
+  document.getElementById('iCompany').value=c;
+}
